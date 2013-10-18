782970e080c7127fff07bc3eca68bc1df6e83112
dodikk <dodikk88.reg@gmail.com>
2013-10-18 14:48:37 +0300

    Fixed initialization script

--- GetIosSdk12.sh
+++ GetIosSdk12.sh
@@ -3,4 +3,7 @@ git clone --recursive https://github.com/Sitecore/sitecore-ios-sdk.git
 cd "$PWD/sitecore-ios-sdk"
     git checkout remotes/origin/sdk1.2
     git checkout -b sdk1.2
+
+    git submodule init
+    git submodule update
 cd "$LAUNCH_DIR"
--- UpdateIosSDK.sh
+++ UpdateIosSDK.sh
@@ -1,2 +1,5 @@
 git fetch origin
 git merge origin/sdk1.2
+
+git submodule init
+git submodule update
